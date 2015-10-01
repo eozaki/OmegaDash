99c89b17b2a628e73760761ac42ac7aa5b284c22
Erick Cordeiro Ozaki <erick@jaya-apps.com>
2015-10-01 20:21:50 -0300

    Adding search location functionalities

--- app/src/main/java/com/eozaki/omegadash/Map.java
+++ app/src/main/java/com/eozaki/omegadash/Map.java
@@ -1,13 +1,22 @@
 package com.eozaki.omegadash;
 
 import android.content.pm.ActivityInfo;
+import android.location.Address;
+import android.location.Geocoder;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class Map extends FragmentActivity {
 
@@ -19,6 +28,26 @@ public class Map extends FragmentActivity {
         setContentView(R.layout.activity_map);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
         setUpMapIfNeeded();
+    }
+
+    public void searchAddress(View v){
+        EditText textBar = (EditText)findViewById(R.id.addressText);
+        String location = textBar.getText().toString();
+        List<android.location.Address> addresses = new ArrayList();
+        if(location != null || !location.equals("")) {
+            Geocoder geo = new Geocoder(this, Locale.getDefault());
+            try {
+                addresses = geo.getFromLocationName(location, 1);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (addresses != null) {
+                Address address = addresses.get(0);
+                LatLng position = new LatLng(address.getLatitude(), address.getLongitude());
+                mMap.addMarker(new MarkerOptions().position(position).title(location));
+
+            }
+        }
     }
 
     @Override
@@ -42,5 +71,6 @@ public class Map extends FragmentActivity {
 
     private void setUpMap() {
         mMap.setMyLocationEnabled(true);
+        mMap.getMyLocation();
     }
 }
